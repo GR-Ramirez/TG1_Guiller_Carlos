64783bd34c2d9d66eb36003c2ed7125034ce7cd8
Guillermo Ramirez Bonilla <boss18_3@hotmail.com>
2011-05-13 07:27:58 -0600

    creado el main

---
+++ src/JavaBank.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author Owner
+ */
+public class JavaBank {
+    static Prestamo prestamos[];
+    static cantPrestamos[];
+}
