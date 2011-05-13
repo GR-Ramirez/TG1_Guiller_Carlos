f94e1cd4a056a80a30241e654b2abad812effb78
Guillermo Ramirez Bonilla <boss18_3@hotmail.com>
2011-05-13 08:39:49 -0600

    deberia ser de los ultimos

--- src/Prestamo.java
+++ src/Prestamo.java
@@ -16,7 +16,19 @@ public abstract class Prestamo {
         montoPrestamo= monto;
         nombre= nom;
         cuotas= new Cuota[numPagos];
+        double mensualBasico= monto/numPagos;
+        double inte= 1+getsaldoPendiente();
+        llenarCuotas(mensualBasico*inte);
     }
-    abstract double  saldoPendiente();
+    abstract double  getsaldoPendiente();
     abstract double  getTasaInteres( );
+
+    public void llenarCuotas(double pg){
+        for(Cuota cuot: cuotas)
+            cuot= new Cuota(pg);
+    }
+
+    public void imprimir(){
+        System.out.println("Nombre de cliente:" + nombre );
+    }
 }
